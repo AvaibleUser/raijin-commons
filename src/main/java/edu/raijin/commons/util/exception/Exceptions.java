e23f5a4d57002ef8e2567e3a2c2eb58993361438
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-12-14 01:04:59 -0600

    feat: add util functions

--- src/main/java/edu/raijin/commons/util/exception/Exceptions.java
+++ src/main/java/edu/raijin/commons/util/exception/Exceptions.java
@@ -1,5 +1,6 @@
 package edu.raijin.commons.util.exception;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public final class Exceptions {
@@ -9,6 +10,46 @@ public final class Exceptions {
             throw messageSupplier.get();
         } else {
             return obj;
+        }
+    }
+
+    public static <T> List<T> requireNonEmpty(List<T> list, Supplier<RuntimeException> messageSupplier) {
+        if (list == null || list.isEmpty()) {
+            throw messageSupplier.get();
+        } else {
+            return list;
+        }
+    }
+
+    public static String requireNonBlank(String str, Supplier<RuntimeException> messageSupplier) {
+        if (str == null || str.isBlank()) {
+            throw messageSupplier.get();
+        } else {
+            return str;
+        }
+    }
+
+    public static <T extends Number> T requireNonNegative(T number, Supplier<RuntimeException> messageSupplier) {
+        if (number == null || number.doubleValue() < 0) {
+            throw messageSupplier.get();
+        } else {
+            return number;
+        }
+    }
+
+    public static <T extends Number> T requirePositive(T number, Supplier<RuntimeException> messageSupplier) {
+        if (number == null || number.doubleValue() <= 0) {
+            throw messageSupplier.get();
+        } else {
+            return number;
+        }
+    }
+
+    public static <T extends Number> T requireNonZero(T number, Supplier<RuntimeException> messageSupplier) {
+        if (number == null || number.doubleValue() == 0) {
+            throw messageSupplier.get();
+        } else {
+            return number;
         }
     }
 }
