daf3de6a55c48878906b8e1cd7e7f472ef8655e5
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-12-21 23:10:03 -0600

    feat: add contract event + status

---
+++ src/main/java/edu/raijin/commons/domain/type/ContractStatus.java
@@ -0,0 +1,8 @@
+package edu.raijin.commons.domain.type;
+
+public enum ContractStatus {
+
+    ACTIVE,
+    SUSPENDED,
+    TERMINATED,
+}
---
+++ src/main/java/edu/raijin/commons/infrastructure/adapter/messaging/event/finance/ContractEvent.java
@@ -0,0 +1,32 @@
+package edu.raijin.commons.infrastructure.adapter.messaging.event.finance;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import edu.raijin.commons.domain.type.ContractStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContractEvent {
+
+    private Long id;
+
+    private UUID employeeId;
+
+    private BigDecimal baseSalary;
+
+    private String role;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private ContractStatus status;
+}
