f3ecc2e9da4777b42cced27d89da450cc3b82c27
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-12-26 18:51:43 -0600

    feat: add sprint + story events

---
+++ src/main/java/edu/raijin/commons/infrastructure/adapter/messaging/event/scrum/SprintEvent.java
@@ -0,0 +1,32 @@
+package edu.raijin.commons.infrastructure.adapter.messaging.event.scrum;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import edu.raijin.commons.domain.type.SprintStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SprintEvent {
+
+    private UUID id;
+
+    private UUID projectId;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Builder.Default
+    private SprintStatus status = SprintStatus.PENDING;
+}
---
+++ src/main/java/edu/raijin/commons/infrastructure/adapter/messaging/event/scrum/StoryEvent.java
@@ -0,0 +1,36 @@
+package edu.raijin.commons.infrastructure.adapter.messaging.event.scrum;
+
+import java.util.UUID;
+
+import edu.raijin.commons.domain.type.StoryPriority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoryEvent {
+
+    private UUID id;
+
+    private UUID projectId;
+
+    private UUID productOwnerId;
+
+    private UUID developerId;
+
+    private UUID sprintId;
+
+    private String name;
+
+    private String description;
+
+    private Integer points;
+
+    private StoryPriority priority;
+
+    private String stage;
+}
